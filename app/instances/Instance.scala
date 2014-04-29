8b4147c65d4a00ab15800298fca7bb240ab3b795
Francisco Arámburo <atfm05@gmail.com>
2014-04-29 12:38:27 -0500

    Issue #6: Improved Instance class.

--- app/instances/Instance.scala
+++ app/instances/Instance.scala
@@ -5,10 +5,10 @@
 package instances
 
 import scala.concurrent._
-import scala.collection.mutable.Queue
 
 import play.api.libs.concurrent.Execution.Implicits._
 
+import controllers.N4j
 import controllers.WebService
 import compute.EngineAlgorithm
 
@@ -16,11 +16,31 @@ class Instance (val h: String, val p: Int) extends WebService {
 
   this.setHost(host, port)
   
-  var activeGraphHost: String = null
+  var activeGraphHost: N4j = null
 
   var activeAlgorithm: String = null
 
   /** Queue for the requests. */
-  var queue: Queue[EngineAlgorithm] = Queue()
+  var queue: Array[EngineAlgorithm] = new Array[EngineAlgorithm](conf.ce_max_queue_size)
+
+  def hasFreeSpace: Boolean = {
+    for (algo: EngineAlgorithm <- queue) {
+      if (algo == null)
+        return true
+    }
+    return false
+  }
+
+  def hasEngineAlgorithmRequest (token: String): Boolean = {
+    for (algo: EngineAlgorithm <- queue) {
+      if (algo != null && algo.token == token)
+        return true
+    }
+    return false
+  }
+
+  //def enqueue 
+
+  //def finishedEngineAlgorithmRequest
 
 }

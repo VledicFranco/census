7c4d73b7963777df3fa47c86723fe1202f8a4c86
Francisco Arámburo <atfm05@gmail.com>
2014-05-19 19:28:50 -0500

    Issue #10: Necessary code for http hook reports.

--- app/controllers/HTTPHook.scala
+++ app/controllers/HTTPHook.scala
@@ -4,6 +4,8 @@
 
 package controllers
 
+import com.github.nscala_time.time.Imports._
+
 import play.api.libs.json._
 import play.api.libs.ws._
 import play.api.libs.concurrent.Execution.Implicits._
@@ -19,16 +21,39 @@ import requests._
  */
 object HTTPHook extends WebService {
 
-  def isActive: Boolean = (host != "" && port != 0) 
+  def report (token: String): Unit = {
+    if (host == "unset") return
+    post("/censuscontrol/report", "{"
+      +s""" "token": "$token", """
+      + """ "status": "success" """
+      + "}"
+    ) recover {
+      case _ => println(s"${DateTime.now} - WARNING: Unreachable HTTP hook server.")
+    }
+  }
+
+  def error (token: String, error: String, on: String): Unit = {
+    if (host == "unset") return
+    post("/censuscontrol/error", "{"
+      +s""" "token": "$token", """
+      + """ "status": "error", """
+      +s""" "error": "$error", """
+      +s""" "on": "$on" """
+      + "}"
+    ) recover {
+      case _ => println(s"${DateTime.now} - WARNING: Unreachable HTTP hook server.")
+    }
+  }
 
   /**
    * Success reports.
    */
   object Report {
   
-//    def computationFinished (request: ComputationRequest): Unit = {
-//      println(s"Computation with token:${request.token} finished.")
-//    }
+    def computationFinished (request: ComputationRequest): Unit = {
+      report(request.token)
+      println(s"${DateTime.now} - REPORT: Computation with token:${request.token} finished in: ${request.computationTime} ms.")
+    }
 
   }
 
@@ -37,15 +62,6 @@ object HTTPHook extends WebService {
    */
   object Error {
     
-    def invalidN4jFormat (request: ComputationRequest): Unit = {
-      println("Invalid Neo4j format.") 
-    }
-
-    def unreachableN4j (request: ComputationRequest): Unit = {
-      if (isActive)
-        println("Unreachable Neo4j server.")
-    }
-
   }
 
 }
